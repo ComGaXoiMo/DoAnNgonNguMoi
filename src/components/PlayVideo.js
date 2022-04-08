3a60ea5abb33fdb5bba1d74c8d3af5db3384530a
ComGaXoiMo <vietng50@gmail.com>
2022-04-08 21:01:30 +0700

    xong goi 1 phim

--- src/components/PlayVideo.js
+++ src/components/PlayVideo.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect  } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import Header from './Header';
 import { Link } from 'react-router-dom';
 import Footer from './Footer';
@@ -13,34 +13,65 @@ import Filmvideo from './filminfo/Filmvideo';
 import VideoPlayer from 'react-video-js-player';
 import link from "../assets/images/film1.mp4"
 const PlayVideo = () => {
-    
+
     const [items, setItems] = useState([]);
     const videosrc = link;
     const poster = "../assets/images/movies/call.jpg";
     useEffect(() => {
         const fetchItems = async () => {
-            // fix lỗi CRORS
-            //https://www.breakingbadapi.com/api/characters
-            //http://localhost:5000/api/film/listallfilm
-
             const result = await axios.get(`http://localhost:5000/api/film/listallfilm`);
-           // console.log(result.data)
+
             setItems(result.data)
-            
-            
-           
         }
         fetchItems()
     }, [])
     return (
         <><Header />
-        
-        <Filmvideo  items = {items}/>
-      
-   
-           
+        <div class="row">
+            <div class="col ">
+                <hr></hr>
+                <Filmvideo items={items} />
+                <hr></hr>
+                
+                
+            </div>
+            <div class="col">
+                {/*nhap comment */}
+                <hr></hr>
+                <form>
+                    <div class=" row" >
+                        <label class=" col-sm-3" >Tên</label>
+                    </div>
+                    <div class=" row" >
+                        <label class=" col-sm-3" >Bình luận:</label>
+                        <input class=" col-sm-6" type="text" name="commnent" placeholder="Nhập bình luận " />
+                        <input type="submit" class="btngbl col-sm-2 " value="Gửi" />
+                    </div>
+                </form>
+                <hr></hr>
+                {/* coment dã co */}
+                <div>
+                    <div class="row" >
+                        <label class=" col-sm-3" >Duc nguyen duy duc :</label>
+                        <label class=" col-sm-9" >The interactive example below demonstrates some of the values for align-items using grid layouthay ay hay</label>
+                    </div>
+                    <div class="row" >
+                        <label class=" col-sm-3" >Duc :</label>
+                        <label class=" col-sm-9" >The interactive example below demonstrates some of the values for align-items using grid layouthay ay hay</label>
+                    </div>
+                    <div class="row" >
+                        <label class=" col-sm-3" >Duc :</label>
+                        <label class=" col-sm-9" >The interactive example below demonstrates some of the values for align-items using grid layouthay ay hay</label>
+                    </div>
+                    <div class="row" >
+                        <label class=" col-sm-3" >Duc :</label>
+                        <label class=" col-sm-9" >The interactive example below demonstrates some of the values for align-items using grid layouthay ay hay</label>
+                    </div>
+                </div>
+            </div>
+</div>
             <Footer />
-            </>
+        </>
     );
 }
 export default PlayVideo;
--- src/components/filminfo/filmvideo.js
+++ src/components/filminfo/filmvideo.js
@@ -1,36 +1,44 @@
 import React, { Component } from 'react';
 
 import { Link } from 'react-router-dom';
-import {useParams} from 'react-router-dom';
-// import '../css/site.css';
-// import '../css/style.css';
+import { useParams } from 'react-router-dom';
+ import '../../assets/app.css';
+ import '../../css/style.css';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import Filmitem from './Filmitem';
 import VideoPlayer from 'react-video-js-player';
- //import link from "http://localhost:5001/video1?v=film1"
-    
-    const Filmvideo = ({items}) => {
-      const {id} = useParams();
-        console.log(items)
-        return (
-        <section>
+//import link from "http://localhost:5001/video1?v=film1"
+
+const Filmvideo = ({ items }) => {
+  const { id } = useParams();
+  console.log(items)
+  return (
+    <section>
+
+      {
+        items.map((item) => {
+          if (item._id == id) {
+            return(
+              <>
+              <video id="videoPlayer" width="650" height="450" controls muted="muted" autoplay>
+
+              <source src={"http://localhost:5001/video1?v=" + item.videoLink} type="video/mp4" />
+            </video>
+            <label class="section-header">{item.nameMovie} </label>
             
-             {
-             items.map((item) =>(
-               
-                //  <VideoPlayer key={item._id} src={"http://localhost:5001/video1?v=film1"}  poster={item.poster} width="700" height="450" ></VideoPlayer>
-                <video  id="videoPlayer" width="650" height="450" controls muted="muted" autoplay>
-                  
-                <source src={"http://localhost:5001/video1?v="+ item.videoLink} type="video/mp4" />
-              </video>
+              </>
+            
             )
-            )}
-                            
-        </section> 
-      
-        )
-      }
-      
+            //  <VideoPlayer key={item._id} src={"http://localhost:5001/video1?v=film1"}  poster={item.poster} width="700" height="450" ></VideoPlayer>
+          }
+        }
+        )}
+
+    </section>
+
+  )
+}
+
 //}
 export default Filmvideo;
